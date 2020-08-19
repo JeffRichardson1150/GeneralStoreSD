66f801e12437f5b4564e79e5914da26efe49bc9d
JeffRichardson1150 <jeffrichardson573@gmail.com>
2020-08-18 22:07:41 -0400

    Add project files.

---
+++ GeneralStore.MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Thank you for visiting General Store";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Please let us know how you've enjoyed General Store";
+
+            return View();
+        }
+    }
+}
